58c71837c177368f5a9d1e499d281b7a6b202a51
GaiusMartius <ironapple@gmail.com>
2017-04-30 14:57:57 -0400

    Removing comments from example middleware

--- middleware.go
+++ middleware.go
@@ -8,13 +8,7 @@ import (
 func CORSHandler(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Cookies")
-		for _, cookie := range r.Cookies() {
-			fmt.Fprint(w, cookie.Name)
-		}
 
-		fmt.Println("CORSHandler")
-		// http.SetCookie(w, &http.Cookie{Name:"Set-Cookie", Value:"a=b"})
 		origin := r.Header.Get("Origin")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Origin", origin)
