c90c65c8b4afa8648376015200ebd6959d457c17
GaiusMartius <ironapple@gmail.com>
2017-05-03 14:11:17 -0400

    Package layout along standard lines

--- cmd/commento/main.go
+++ cmd/commento/main.go
@@ -1,63 +1,59 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/op/go-logging"
+	
+	. "github.com/adtac/commento/lib"
 )
 
-var logger = logging.MustGetLogger("commento")
-var db *sql.DB
-
 func main() {
-	err := loadDatabase("sqlite3.db")
+	err := LoadDatabase("sqlite3.db")
 	if err != nil {
-		die(err)
+		Die(err)
 	}
-
+	
 	fs := http.FileServer(http.Dir("assets"))
-
+	
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/create", createCommentHandler)
-	http.HandleFunc("/get", getCommentsHandler)
-
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/create", CreateCommentHandler)
+	http.HandleFunc("/get", GetCommentsHandler)
+	
 	var port string
-
+	
 	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
 		port = ":" + fromEnv
 	} else {
 		port = ":8080"
 	}
-
+	
 	if demoEnv := os.Getenv("DEMO"); demoEnv == "true" {
 		fmt.Println("clearing")
 		go func() {
 			for true {
-				err := cleanupOldComments()
+				err := CleanupOldComments()
 				if err != nil {
-					logger.Errorf("Error cleaning up old comments %s", err)
+					Logger.Errorf("Error cleaning up old comments %s", err)
 				}
 				time.Sleep(60 * time.Second)
 				fmt.Println("deleting")
 			}
 		}()
 	}
-
+	
 	svr := &http.Server{
 		Addr:         port,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
-	logger.Infof("Running on port %s", port)
+	Logger.Infof("Running on port %s", port)
 	err = svr.ListenAndServe()
 	if err != nil {
-		logger.Fatalf("http.ListenAndServe: %v", err)
+		Logger.Fatalf("http.ListenAndServe: %v", err)
 	}
-
 }
---
+++ lib/utils.go
@@ -0,0 +1,17 @@
+package lib
+
+import (
+	"fmt"
+
+	"github.com/op/go-logging"
+)
+
+var Logger = logging.MustGetLogger("commento")
+
+func Emit(err error) {
+	fmt.Printf("%s", fmt.Errorf("%s", err))
+}
+
+func Die(err error) {
+	Logger.Panic(fmt.Errorf("%s", err))
+}
