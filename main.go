fcc8b2d06e63dab95b08f148b800dcec0bf98d17
GaiusMartius <ironapple@gmail.com>
2017-05-07 23:47:17 -0400

    Configuring startup parameters from environment. Defaults are included in cmd/commento/.env.development

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/joho/godotenv"
+	
+	. "github.com/adtac/commento/lib"
 )
 
 func main() {
@@ -13,7 +16,15 @@ func main() {
 	if err != nil {
 		Die(err)
 	}
-	
+
+	// Load configuration from the environment.
+	// Values in earlier files will take precedence over later values
+	//    Ex. A COMMENTO_PORT value in .env.development.local will be used
+	//        even if COMMENTO_PORT is exists in a .env.development file
+	for _, envFile := range []string{".env.development.local", ".env.test.local", ".env.production.local", ".env.local", ".env.development", ".env.test", ".env.production", ".env"} {
+		godotenv.Load(envFile)
+	}
+
 	fs := http.FileServer(http.Dir("assets"))
 	
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
@@ -21,15 +32,7 @@ func main() {
 	http.HandleFunc("/create", CreateCommentHandler)
 	http.HandleFunc("/get", GetCommentsHandler)
 	
-	var port string
-	
-	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
-		port = ":" + fromEnv
-	} else {
-		port = ":8080"
-	}
-	
-	if demoEnv := os.Getenv("DEMO"); demoEnv == "true" {
+	if os.Getenv("COMMENTO_DEMO") == "true" {
 		t := time.Second * 60
 		Logger.Infof("Demo Env: Cleaning old comments every %s", t)
 		go func() {
@@ -43,8 +46,10 @@ func main() {
 		}()
 	}
 	
+	port := os.Getenv("COMMENTO_PORT")
+
 	svr := &http.Server{
-		Addr:         port,
+		Addr:         ":" + port,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
