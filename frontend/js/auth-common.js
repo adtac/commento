ee324c1f288298cf28d5066f8b93969ad3204b3c
Adhityaa Chandrasekar <adtac@adtac.in>
2018-08-08 11:38:16 +0530

    login, signup: redirect to dashboard if logged in
    Fixes https://gitlab.com/commento/commento-ce/issues/60

--- frontend/js/auth-common.js
+++ frontend/js/auth-common.js
@@ -1,5 +1,14 @@
 (function (global, document) {
 
+  // Redirect the user to the dashboard if there's a cookie. If the cookie is
+  // invalid, they would be redirected back to the login page *after* the
+  // cookie is deleted.
+  global.loggedInRedirect = function() {
+    if (global.cookieGet("commentoOwnerToken") !== undefined)
+      document.location = "/dashboard";
+  }
+
+
   // Prefills the email field from the URL parameter.
   global.prefillEmail = function() {
     if (paramGet("email") != undefined) {
