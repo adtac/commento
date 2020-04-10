9a4563fdb38063de31e2feae1c0c37d634219ddd
Adhityaa Chandrasekar <adtac@adtac.in>
2020-04-10 17:09:54 -0400

    db: maintain 9.6 compatibility
    I know it's generally frowned upon to make edits to existing migrations,
    but this should be a transparent change that makes absolutely no
    difference to existing users with the migration already applied.
    However, PostgreSQL 9.6 users (still the default on Debian Stretch)
    stand to gain a lot from this simple change.

--- db/20190913175445-delete-comments.sql
+++ db/20190913175445-delete-comments.sql
@@ -1,6 +1,6 @@
 DROP TRIGGER IF EXISTS commentsDeleteTrigger ON comments;
 
-DROP FUNCTION IF EXISTS commentsDeleteTriggerFunction;
+DROP FUNCTION IF EXISTS commentsDeleteTriggerFunction();
 
 ALTER TABLE comments
   ADD deleted BOOLEAN NOT NULL DEFAULT false;
